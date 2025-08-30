14a0923266d2d6a241834a263f9ee04d2e1fbb23
RigobertoVargasGaona <rigovg1054@gmail.com>
2025-08-30 11:48:27 -0500

    configuracion react

---
+++ react/cliente/src/App.js
@@ -0,0 +1,14 @@
+
+import './App.css';
+import Dashboard from './components/pages/Dashboard.js'
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+
+function App() {
+  return (
+    <div className="App">
+     <Dashboard/>
+    </div>
+  );
+}
+
+export default App;
---
+++ react/cliente/src/components/pages/Dashboard.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+
+const Dashboard=()=>{
+    return(
+        <div >
+            <h1>Bienvenidos</h1>
+            <p>Hola</p>
+        </div>
+    );
+}
+
+export default Dashboard;
